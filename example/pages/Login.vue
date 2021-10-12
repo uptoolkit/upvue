317aa22f9299ed706875a84769bcb76cbc5e8239
danielsum <daniel@lacerisenumerique.be>
2021-10-13 00:29:39 +0200

    feat: update logo

--- example/pages/Login.vue
+++ example/pages/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+      <img class="mx-auto h-12 w-auto" src="/img/logo.svg" alt="Workflow" />
       <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
         Sign in to the demo
       </h2>
