140ac70f596273f55f3aa151662727babb3ecc38
danielsum <daniel@lacerisenumerique.be>
2021-10-14 22:37:28 +0200

    feat: publish new release

---
+++ docs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# Push to repository
+git add .
+git status
+git commit -m 'feat: publish new release'
+git push
+
+# Publish the docs
+cd docs
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:uptoolkit/upvue.git master:gh-pages
+
+cd -
--- publish.sh
+++ publish.sh
@@ -23,18 +23,4 @@ git commit -m 'feat: publish new release'
 git push
 
 # Publish the docs
-cd docs
-
-# build
-npm run docs:build
-
-# navigate into the build output directory
-cd .vuepress/dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:uptoolkit/upvue.git master:gh-pages
-
-cd -
+bash docs.sh
