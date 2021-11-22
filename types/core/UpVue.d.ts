f99e27fc81db1b451fca97bd6ba7453a9ed05dbe
danielsum <daniel@lacerisenumerique.be>
2021-11-22 14:16:37 +0100

    feat: improve graphql example

--- types/core/UpVue.d.ts
+++ types/core/UpVue.d.ts
@@ -1,10 +1,13 @@
 import { Ref } from 'vue';
 import type { App } from 'vue';
 import { message, notification } from 'ant-design-vue';
+import { ApolloClient } from '@apollo/client/core';
 import { UseQueryReturn } from '@vue/apollo-composable';
 import { useQuery } from '@vue/apollo-composable';
+import { Config } from 'js-config-helper';
 import { Form } from 'js-form-helper';
-import { Axios } from "axios";
+import { Axios, AxiosInstance } from "axios";
+import I18n from "@cherrypulp/i18n/types/I18n";
 import { Store } from "vuex";
 import { DeepNonNullable, DeepRequired } from "ts-essentials";
 /**
@@ -21,7 +24,7 @@ export interface VueOptions {
     };
     i18n: Record<string, unknown>;
     storeMode: "reactive" | "vuex";
-    store: Store<unknown | object>;
+    store: Store<unknown | object> | object;
     api: {
         url: string;
     };
@@ -44,16 +47,17 @@ export interface graphqlResult<TResult, TDefaultValue, TReturnValue> {
     pick?: (data: DeepRequired<DeepNonNullable<TResult>>) => TReturnValue;
 }
 export interface exportedVars {
-    config: boolean;
-    api: boolean;
-    http: boolean;
-    i18n: boolean;
-    form: Form;
-    formApi: Form;
-    store?: boolean;
+    config: typeof config;
+    api: AxiosInstance;
+    http: AxiosInstance;
+    i18n: typeof I18n;
+    form: typeof Form;
+    formApi: typeof Form;
+    store?: typeof Store | any;
     t?(key: string, data?: object, lang?: string): string | any;
     __?(key: string, data?: object, lang?: string): string | any;
     choice?(key: string, count?: number, data?: any, locale?: string): string | any;
+    graphql: ApolloClient<object>;
     graphqlQuery: graphqlQuery;
     graphqlResult: graphqlResult<any, any, any>;
     message?: typeof message;
@@ -69,6 +73,7 @@ declare global {
         __app: object;
     }
 }
+declare let config: Config;
 /**
  * Access to the instance a a singleton
  */
@@ -76,3 +81,4 @@ export declare let useUp: () => exportedVars;
 export declare const UpVue: {
     install: (app: App, options: VueOptions) => void;
 };
+export {};
