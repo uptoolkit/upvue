e97c488402fc2e4c69e09bb9ce9b92bc1a392d2a
danielsum <daniel@lacerisenumerique.be>
2021-11-24 19:05:07 +0100

    fix: publish new release

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
         "core-js",
         "daisyui",
         "form-backend-validation",
+        "js-form-helper",
         "lodash",
         "moment",
         "tailwindcss",
